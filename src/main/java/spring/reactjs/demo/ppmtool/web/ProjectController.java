5b3f6048dfcd940f1e4754da2b7a4e988b0ac94b
dexterchan <dexterchan@gmail.com>
2019-03-03 20:04:47 +0800

    fix error message

--- src/main/java/spring/reactjs/demo/ppmtool/web/ProjectController.java
+++ src/main/java/spring/reactjs/demo/ppmtool/web/ProjectController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,8 @@ import spring.reactjs.demo.ppmtool.domain.Project;
 import spring.reactjs.demo.ppmtool.services.ProjectService;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -23,7 +26,13 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody  Project project, BindingResult result){
 
         if(result.hasErrors()){
-            return new ResponseEntity<String>("INvalid object", HttpStatus.BAD_REQUEST);
+
+            Map<String,String> errorMap = new HashMap<String,String>();
+            for (FieldError err: result.getFieldErrors()){
+                errorMap.put(err.getField(),err.getDefaultMessage());
+            }
+
+            return new ResponseEntity<  Map<String,String> >(errorMap, HttpStatus.BAD_REQUEST);
         }
         Project prj1=projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(prj1, HttpStatus.CREATED);
