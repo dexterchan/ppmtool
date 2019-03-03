7f2ee8d4df7a0d029d4d1e83bb939f80f579adbb
dexterchan <dexterchan@gmail.com>
2019-03-03 21:06:24 +0800

    custom handler

--- src/main/java/spring/reactjs/demo/ppmtool/services/ProjectService.java
+++ src/main/java/spring/reactjs/demo/ppmtool/services/ProjectService.java
@@ -3,6 +3,7 @@ package spring.reactjs.demo.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.reactjs.demo.ppmtool.domain.Project;
+import spring.reactjs.demo.ppmtool.exceptions.ProjectIdException;
 import spring.reactjs.demo.ppmtool.repositories.ProjectRepository;
 
 @Service
@@ -13,7 +14,12 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project){
         //Logic
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("ProjectID "+project.getProjectIdentifier().toUpperCase()+ " already exists");
+        }
 
-        return projectRepository.save(project);
     }
 }
